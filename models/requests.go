b147380f0ee8417555c4451b28a7cbcdc318163b
kaiser suzaku <68168975+kaisersuzaku@users.noreply.github.com>
2020-10-17 09:36:27 +0700

    Create minimum FillBallContainer models

---
+++ models/requests.go
@@ -0,0 +1,20 @@
+package models
+
+type InvalidPayload struct {
+	ErrorCode    string `json:"error_code"`
+	ErrorMessage string `json:"error_message"`
+}
+
+type BallContainer struct {
+	BallContainerSize      int `json:"ball_container_size"`
+	CurrentBallInContainer int `json:"current_ball_in_container"`
+}
+
+type ThrownBall struct {
+	Ball int `json:"ball"`
+}
+
+type FillBallContainerReq struct {
+	BallContainer BallContainer `json:"ball_container"`
+	ThrownBall    ThrownBall    `json:"thrown_ball"`
+}
