0e207a7bf842f0403d2610164a0df10fe1685dc7
kaiser suzaku <68168975+kaisersuzaku@users.noreply.github.com>
2020-10-17 11:34:34 +0700

    Create bare minimum FillBallContainerService and Unit test (positive case)

---
+++ services/fill_ball_container.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"context"
+
+	"github.com/kaisersuzaku/BE_A/models"
+)
+
+const (
+	ContainerAlreadyFull string = "ALREADY_FULL"
+
+	// ContainerVerified : number of current ball in container after addition is same with ball container size
+	ContainerVerified string = "VERIFIED"
+	ContainerNotFull  string = "NOT_FULL"
+)
+
+const (
+	CommonBallContainerSizeFour  int = 4
+	CommonBallContainerSizeThree int = 3
+)
+
+type FillBallContainerService struct {
+}
+
+type IFillBallContainerService interface {
+	IsContainerFull(ctx context.Context, req models.FillBallContainerReq) models.FillBallContainerResp
+}
+
+func (fbcs FillBallContainerService) IsContainerFull(ctx context.Context, req models.FillBallContainerReq) (resp models.FillBallContainerResp) {
+	totalBall := req.BallContainer.CurrentBallInContainer + req.ThrownBall.NumberOfBall
+	if totalBall == req.BallContainer.BallContainerSize {
+		resp.Status = ContainerVerified
+		resp.BallContainer = req.BallContainer
+		resp.BallContainer.CurrentBallInContainer = totalBall
+		return
+	}
+	return
+}
---
+++ services/fill_ball_container_test.go
@@ -0,0 +1,102 @@
+package services_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kaisersuzaku/BE_A/models"
+	"github.com/kaisersuzaku/BE_A/services"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBallContainerSizeFour(t *testing.T) {
+	assert := assert.New(t)
+
+	fillBallContainerService := services.FillBallContainerService{}
+	var tests = []struct {
+		testName string
+		input1   func() context.Context
+		input2   func() models.FillBallContainerReq
+		expected func() models.FillBallContainerResp
+	}{
+		{
+			"Ball Container VERIFIED",
+			func() context.Context {
+				return context.TODO()
+			},
+			func() models.FillBallContainerReq {
+				ballContainer := models.BallContainer{
+					BallContainerSize:      services.CommonBallContainerSizeFour,
+					CurrentBallInContainer: 3,
+				}
+				thrownBall := models.ThrownBall{
+					NumberOfBall: 1,
+				}
+				return models.FillBallContainerReq{
+					BallContainer: ballContainer,
+					ThrownBall:    thrownBall,
+				}
+			},
+			func() models.FillBallContainerResp {
+				ballContainer := models.BallContainer{
+					BallContainerSize:      services.CommonBallContainerSizeFour,
+					CurrentBallInContainer: services.CommonBallContainerSizeFour,
+				}
+				return models.FillBallContainerResp{
+					BallContainer: ballContainer,
+					Status:        services.ContainerVerified,
+				}
+			},
+		},
+	}
+	for _, test := range tests {
+		resp := fillBallContainerService.IsContainerFull(test.input1(), test.input2())
+		assert.Equal(test.expected(), resp)
+	}
+}
+
+func TestBallContainerSizeThree(t *testing.T) {
+	assert := assert.New(t)
+
+	fillBallContainerService := services.FillBallContainerService{}
+	var tests = []struct {
+		testName string
+		input1   func() context.Context
+		input2   func() models.FillBallContainerReq
+		expected func() models.FillBallContainerResp
+	}{
+		{
+			"Ball Container VERIFIED",
+			func() context.Context {
+				return context.TODO()
+			},
+			func() models.FillBallContainerReq {
+				ballContainer := models.BallContainer{
+					BallContainerSize:      services.CommonBallContainerSizeThree,
+					CurrentBallInContainer: 2,
+				}
+				thrownBall := models.ThrownBall{
+					NumberOfBall: 1,
+				}
+				return models.FillBallContainerReq{
+					BallContainer: ballContainer,
+					ThrownBall:    thrownBall,
+				}
+			},
+			func() models.FillBallContainerResp {
+				ballContainer := models.BallContainer{
+					BallContainerSize:      services.CommonBallContainerSizeThree,
+					CurrentBallInContainer: services.CommonBallContainerSizeThree,
+				}
+				return models.FillBallContainerResp{
+					BallContainer: ballContainer,
+					Status:        services.ContainerVerified,
+				}
+			},
+		},
+	}
+	for _, test := range tests {
+		resp := fillBallContainerService.IsContainerFull(test.input1(), test.input2())
+		assert.Equal(test.expected(), resp)
+	}
+}
