026a8a49b77ecf5a081c42d2ae60fbbb6795a487
kaiser suzaku <68168975+kaisersuzaku@users.noreply.github.com>
2020-10-19 11:13:04 +0700

    Fix FillBallContainerReq payload valid requirement

--- models/requests.go
+++ models/requests.go
@@ -1,9 +1,9 @@
 package models
 
 type BallContainer struct {
-	BallContainerSize      int `json:"ball_container_size" valid:"required"`
-	CurrentBallInContainer int `json:"current_ball_in_container" valid:"required"`
-	ID                     int `json:"id" valid:"required"`
+	BallContainerSize      int  `json:"ball_container_size" valid:"range(1|4)"`
+	CurrentBallInContainer int  `json:"current_ball_in_container" valid:"range(0|4)"`
+	ID                     uint `json:"id" valid:"required"`
 }
 
 type FillBallContainerReq struct {
